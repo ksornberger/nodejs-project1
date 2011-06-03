b6c9a613b31bfb999a862588c167add28f4547f9
Kevin Sornberger <ksornberger@gmail.com>
2011-06-03 11:29:23 -0400

    Blocking code example

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,21 @@
+//var exec = require("child_process").exec;d
+
 function start(){
 	console.log("Request handler 'start' was called.");
+	
+	function sleep(milliSeconds) {
+		var startTime = new Date().getTime();	// get the current time
+		while (new Date().getTime() < startTime + milliSeconds); // hog cpu!
+	}
+	
+	sleep (10000);
+	return "Hello Start";
 }
 
 function upload(){
 	console.log("Request handler 'upload was called.");
+	
+	return "Hello Upload";
 }
 
 exports.start = start;
